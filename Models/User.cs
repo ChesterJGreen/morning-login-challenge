6dad6ce99dac5cf94ea7cbdb499e48c2d856948b
cjgreenie <seejarom@gmail.com>
2021-08-31 10:10:04 -0600

    login challenge

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+namespace user_login.Models
+{
+  public class User
+  {
+    public string Name { get; set; }
+    private string Password { get; set; }
+
+    public bool ValidatePassword(string input)
+    {
+      return input == Password;
+    }
+    public User(string name, string password)
+    {
+      Name = name;
+      Password = password;
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using user_login.Models;
+
+namespace morning_login_challenge
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+      Dictionary<String, User> users = new Dictionary<string, User>();
+      //Make some users
+      User jake = new User("Jake", "IHeartCode!");
+      User jbalvin = new User("JBalvin", "IHeartRocketMortgage!");
+      User mark = new User("Mark", "C0deRox?");
+      User seej = new User("Seej", "MyPasswordDude");
+
+      users.Add(jake.Name, jake);
+      users.Add(jbalvin.Name, jbalvin);
+      users.Add(mark.Name, mark);
+      users.Add(seej.Name, seej);
+
+      bool authenticated = false;
+      while (!authenticated)
+      {
+
+
+        Console.WriteLine("Welcome, please enter the following information");
+        Console.WriteLine("Username: ?");
+        string user = Console.ReadLine();
+        Console.WriteLine("Password: ?");
+        string pWord = Console.ReadLine();
+        if (users.ContainsKey(user) && users[user].ValidatePassword(pWord))
+        {
+          Console.WriteLine("Successfully Logged in");
+          Console.WriteLine("Welcome " + user);
+          authenticated = true;
+
+
+        }
+        else
+        {
+          Console.WriteLine("Invalid username or password...");
+        }
+
+      }
+    }
+  }
+}
